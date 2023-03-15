937790cf451ae821ee1fe5f1078efbc682c937c2
Amal pk <amalashok7902@gmail.com>
2023-03-15 12:16:51 +0530

    Account

--- lib/APP/Account/View/account_page.dart
+++ lib/APP/Account/View/account_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class AccountPage extends StatelessWidget {
   const AccountPage({super.key});
@@ -6,7 +7,197 @@ class AccountPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       backgroundColor: Colors.purple,
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              color: Colors.white,
+              height: 450,
+              child: Stack(
+                children: [
+                  Container(
+                    height: 250,
+                    alignment: Alignment.bottomRight,
+                    decoration: const BoxDecoration(
+                      color: Colors.grey,
+                    ),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.add_a_photo_outlined,
+                        size: 30,
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    right: 250,
+                    // top: 50,
+                    bottom: 100,
+                    left: 0,
+                    child: Container(
+                      height: 150,
+                      alignment: Alignment.center,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.grey,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black,
+                            blurRadius: 4,
+                          ),
+                        ],
+                      ),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.add_a_photo_outlined,
+                          size: 30,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const Positioned(
+                    right: 100,
+                    bottom: 110,
+                    left: 180,
+                    child: Text(
+                      "Bshoot \nVallarpadam,\nErnakulam,Kerala",
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const Text(
+                    "Create Review",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const Text(
+                    "Edit your profile",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 40),
+              height: 20,
+              color: Colors.grey[300],
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 10),
+              // padding: EdgeInsets.symmetric(vertical: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Impact",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      children: [
+                        SvgPicture.asset(
+                          "asset/image/like-svgrepo-com.svg",
+                          height: 50,
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: Text(
+                            "0",
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 40),
+              height: 20,
+              color: Colors.grey[300],
+            ),
+            Container(
+              margin: EdgeInsets.all(16),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: const [
+                      Text(
+                        "Community activity",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      Text(
+                        "View : All activity",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 16, bottom: 16),
+                    child: Text(
+                      "0",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  Text(
+                    "Reviews",
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                        color: Colors.grey[400]),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/APP/Cart/View/cart_page.dart
+++ lib/APP/Cart/View/cart_page.dart
@@ -15,109 +15,132 @@ class CartPage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                alignment: Alignment.center,
-                child: Image(
-                  image: AssetImage(
-                    controller.isBlack
-                        ? controller.tShirt[0]
-                        : controller.isRed
-                            ? controller.tShirt[1]
-                            : controller.tShirt[2],
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 20, bottom: 20),
+                  alignment: Alignment.center,
+                  child: Image(
+                    image: AssetImage(
+                      controller.isBlack
+                          ? controller.tShirt[0]
+                          : controller.isRed
+                              ? controller.tShirt[1]
+                              : controller.tShirt[2],
+                    ),
                   ),
                 ),
-              ),
-              Text(
-                controller.isBlack
-                    ? "Black T shirt"
-                    : controller.isRed
-                        ? "Red T shirt"
-                        : "Green T shirt",
-                style: const TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+                const Divider(
+                  thickness: 1,
                 ),
-              ),
-              const Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Text(
-                  "Colors",
+                const SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  controller.isBlack
+                      ? "Black T shirt"
+                      : controller.isRed
+                          ? "Red T shirt"
+                          : "Green T shirt",
+                  style: const TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 16.0, bottom: 16),
+                  child: Text(
+                    "Colors",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        controller.isBlackColor();
+                      },
+                      icon: Icon(
+                        controller.isBlack
+                            ? Icons.circle_outlined
+                            : Icons.circle,
+                        color: Colors.black,
+                        size: 30,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        controller.isRedColor();
+                      },
+                      icon: Icon(
+                        controller.isRed ? Icons.circle_outlined : Icons.circle,
+                        color: Colors.red,
+                        size: 30,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        controller.isGreenColor();
+                      },
+                      icon: Icon(
+                        controller.isGreen
+                            ? Icons.circle_outlined
+                            : Icons.circle,
+                        color: Colors.green,
+                        size: 30,
+                      ),
+                    )
+                  ],
+                ),
+                const Divider(
+                  thickness: 1,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  "Size",
                   style: TextStyle(
                     fontSize: 24,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-              Row(
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      controller.isBlackColor();
+                SizedBox(
+                  height: 50,
+                  child: ListView.builder(
+                    shrinkWrap: true,
+                    primary: false,
+                    scrollDirection: Axis.horizontal,
+                    itemCount: controller.sizes.length,
+                    itemBuilder: (context, index) {
+                      return Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 8.0,
+                              vertical: 5,
+                            ),
+                            child: Text(
+                              controller.sizes[index],
+                              style: const TextStyle(fontSize: 20),
+                            ),
+                          )
+                        ],
+                      );
                     },
-                    icon: Icon(
-                      controller.isBlack ? Icons.circle_outlined : Icons.circle,
-                      color: Colors.black,
-                      size: 30,
-                    ),
                   ),
-                  IconButton(
-                    onPressed: () {
-                      controller.isRedColor();
-                    },
-                    icon: Icon(
-                      controller.isRed ? Icons.circle_outlined : Icons.circle,
-                      color: Colors.red,
-                      size: 30,
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      controller.isGreenColor();
-                    },
-                    icon: Icon(
-                      controller.isGreen ? Icons.circle_outlined : Icons.circle,
-                      color: Colors.green,
-                      size: 30,
-                    ),
-                  )
-                ],
-              ),
-              const Text(
-                "Size",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
                 ),
-              ),
-              SizedBox(
-                height: 50,
-                child: ListView.builder(
-                  shrinkWrap: true,
-                  primary: false,
-                  scrollDirection: Axis.horizontal,
-                  itemCount: controller.sizes.length,
-                  itemBuilder: (context, index) {
-                    return Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 8.0,
-                            vertical: 5,
-                          ),
-                          child: Text(
-                            controller.sizes[index],
-                            style: const TextStyle(fontSize: 20),
-                          ),
-                        )
-                      ],
-                    );
-                  },
+                const Divider(
+                  thickness: 1,
                 ),
-              )
-            ],
+              ],
+            ),
           ),
         ),
       ),

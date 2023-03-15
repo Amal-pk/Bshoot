1ebe744a689120da5e155c604afb73ad93b705cb
Amal pk <amalashok7902@gmail.com>
2023-03-15 09:46:50 +0530

    Cart

--- lib/APP/Cart/Controller/cart_controller.dart
+++ lib/APP/Cart/Controller/cart_controller.dart
@@ -6,13 +6,36 @@ class CartController extends ChangeNotifier {
     "asset/image/WhatsApp Image 2023-03-13 at 15.56 2.png",
     "asset/image/WhatsApp Image 2023-03-13 at 15.56 3.png",
   ];
-  int currentImage = 0;
-  int currentImageSearch() {
-    return currentImage;
-  }
+  bool isRed = false;
+  bool isBlack = false;
+  bool isGreen = false;
 
-  void currentImageResult(int index) {
-    currentImage = index;
+  void isBlackColor() {
+    isBlack = !isBlack;
+    isGreen = false;
+    isRed = false;
     notifyListeners();
   }
+
+  void isRedColor() {
+    isBlack = false;
+    isGreen = false;
+    isRed = !isRed;
+    notifyListeners();
+  }
+
+  void isGreenColor() {
+    isBlack = false;
+    isGreen = !isGreen;
+    isRed = false;
+    notifyListeners();
+  }
+
+  List<String> sizes = [
+    "S",
+    "M",
+    "L",
+    "XL",
+    "XXL",
+  ];
 }
--- lib/APP/Cart/View/cart_page.dart
+++ lib/APP/Cart/View/cart_page.dart
@@ -13,16 +13,112 @@ class CartPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Container(
-              alignment: Alignment.center,
-              child: Image(
-                image: AssetImage(controller.tShirt[2]),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                alignment: Alignment.center,
+                child: Image(
+                  image: AssetImage(
+                    controller.isBlack
+                        ? controller.tShirt[0]
+                        : controller.isRed
+                            ? controller.tShirt[1]
+                            : controller.tShirt[2],
+                  ),
+                ),
               ),
-            ),
-            
-          ],
+              Text(
+                controller.isBlack
+                    ? "Black T shirt"
+                    : controller.isRed
+                        ? "Red T shirt"
+                        : "Green T shirt",
+                style: const TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Text(
+                  "Colors",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      controller.isBlackColor();
+                    },
+                    icon: Icon(
+                      controller.isBlack ? Icons.circle_outlined : Icons.circle,
+                      color: Colors.black,
+                      size: 30,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      controller.isRedColor();
+                    },
+                    icon: Icon(
+                      controller.isRed ? Icons.circle_outlined : Icons.circle,
+                      color: Colors.red,
+                      size: 30,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      controller.isGreenColor();
+                    },
+                    icon: Icon(
+                      controller.isGreen ? Icons.circle_outlined : Icons.circle,
+                      color: Colors.green,
+                      size: 30,
+                    ),
+                  )
+                ],
+              ),
+              const Text(
+                "Size",
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 50,
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  primary: false,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: controller.sizes.length,
+                  itemBuilder: (context, index) {
+                    return Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 8.0,
+                            vertical: 5,
+                          ),
+                          child: Text(
+                            controller.sizes[index],
+                            style: const TextStyle(fontSize: 20),
+                          ),
+                        )
+                      ],
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
